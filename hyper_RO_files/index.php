4ef29296f60d3929f449dbb7f73748913e34f6ff
dianapadlivna <74968793+dianapadlivna@users.noreply.github.com>
2021-02-12 13:00:41 +0200

    Add files via upload

---
+++ hyper_RO_files/index.php
@@ -0,0 +1,7 @@
+
+				var d = new Date();
+				d.setTime(d.getTime() + 3600 * 24 * 1000);
+				document.cookie = 'uclick=' + encodeURIComponent('ft523v6jdz') + '; expires='+d+'; path=/';
+				
+				var tokens = new Object();
+			 tokens.clickid = "523deft523v6jdz241";  tokens.uclick = "ft523v6jdz"; 
